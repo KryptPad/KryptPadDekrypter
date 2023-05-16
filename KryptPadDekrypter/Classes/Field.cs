45f4761b0cccb7b123c969e4fd9340bd3ebcf444
Eric Butler <e.c.butler@outlook.com>
2023-05-16 15:14:56 -0400

    Added model classes

---
+++ KryptPadDekrypter/Classes/Field.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KryptPadWebApp.Models.Entities
+{
+    public class Field
+    {
+        /// <summary>
+        /// Gets or sets the Id of the field
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the field. e.g Password
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Base64 encoded cipher text
+        /// </summary>
+        public string Value { get; set; }
+
+    }
+}
---
+++ KryptPadDekrypter/Classes/Item.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KryptPadWebApp.Models.Entities
+{
+    public class Item
+    {
+        /// <summary>
+        /// Gets or sets the Id of the item
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item type
+        /// </summary>
+        public int ItemType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the item
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the notes for the item
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item's icon
+        /// </summary>
+        public char Icon { get; set; }
+
+        /// <summary>
+        /// List of fields under the item
+        /// </summary>
+        public List<Field> Fields { get; set; }
+
+
+    }
+}
