8424c560698f0a881837388d48054fb2a374692a
Eric Butler <ebutler@striven.com>
2023-05-19 16:44:07 -0400

    UI Updates and added file icon

--- KryptPadDekrypter/DekryptForm.Designer.cs
+++ KryptPadDekrypter/DekryptForm.Designer.cs
@@ -28,13 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DekryptForm));
             this.label1 = new System.Windows.Forms.Label();
             this.SelectFileButton = new System.Windows.Forms.Button();
-            this.DecryptButton = new System.Windows.Forms.Button();
             this.DecryptedContentsTextBox = new System.Windows.Forms.TextBox();
-            this.DecryptToFileButton = new System.Windows.Forms.Button();
-            this.PassphraseTextBox = new System.Windows.Forms.TextBox();
+            this.FileControlGroupBox = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.PassphraseTextBox = new System.Windows.Forms.TextBox();
+            this.DecryptToFileButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.FileControlGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -48,7 +51,7 @@
             // 
             // SelectFileButton
             // 
-            this.SelectFileButton.Location = new System.Drawing.Point(12, 28);
+            this.SelectFileButton.Location = new System.Drawing.Point(12, 50);
             this.SelectFileButton.Name = "SelectFileButton";
             this.SelectFileButton.Size = new System.Drawing.Size(91, 23);
             this.SelectFileButton.TabIndex = 1;
@@ -56,77 +59,87 @@
             this.SelectFileButton.UseVisualStyleBackColor = true;
             this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
             // 
-            // DecryptButton
-            // 
-            this.DecryptButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.DecryptButton.Enabled = false;
-            this.DecryptButton.Location = new System.Drawing.Point(413, 28);
-            this.DecryptButton.Name = "DecryptButton";
-            this.DecryptButton.Size = new System.Drawing.Size(75, 23);
-            this.DecryptButton.TabIndex = 3;
-            this.DecryptButton.Text = "Dekrypt";
-            this.DecryptButton.UseVisualStyleBackColor = true;
-            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
-            // 
             // DecryptedContentsTextBox
             // 
             this.DecryptedContentsTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.DecryptedContentsTextBox.Location = new System.Drawing.Point(12, 57);
+            this.DecryptedContentsTextBox.Location = new System.Drawing.Point(12, 95);
             this.DecryptedContentsTextBox.Multiline = true;
             this.DecryptedContentsTextBox.Name = "DecryptedContentsTextBox";
             this.DecryptedContentsTextBox.ReadOnly = true;
             this.DecryptedContentsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.DecryptedContentsTextBox.Size = new System.Drawing.Size(610, 342);
+            this.DecryptedContentsTextBox.Size = new System.Drawing.Size(610, 304);
             this.DecryptedContentsTextBox.TabIndex = 4;
+            this.DecryptedContentsTextBox.Text = "Select a file and decrypt it to view its contents.";
             // 
-            // DecryptToFileButton
+            // FileControlGroupBox
             // 
-            this.DecryptToFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.DecryptToFileButton.Enabled = false;
-            this.DecryptToFileButton.Location = new System.Drawing.Point(494, 28);
-            this.DecryptToFileButton.Name = "DecryptToFileButton";
-            this.DecryptToFileButton.Size = new System.Drawing.Size(128, 23);
-            this.DecryptToFileButton.TabIndex = 5;
-            this.DecryptToFileButton.Text = "Dekrypt To File...";
-            this.DecryptToFileButton.UseVisualStyleBackColor = true;
-            // 
-            // PassphraseTextBox
-            // 
-            this.PassphraseTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.FileControlGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.PassphraseTextBox.Enabled = false;
-            this.PassphraseTextBox.Location = new System.Drawing.Point(218, 28);
-            this.PassphraseTextBox.Name = "PassphraseTextBox";
-            this.PassphraseTextBox.Size = new System.Drawing.Size(189, 23);
-            this.PassphraseTextBox.TabIndex = 6;
-            this.PassphraseTextBox.UseSystemPasswordChar = true;
+            this.FileControlGroupBox.Controls.Add(this.label2);
+            this.FileControlGroupBox.Controls.Add(this.PassphraseTextBox);
+            this.FileControlGroupBox.Controls.Add(this.DecryptToFileButton);
+            this.FileControlGroupBox.Controls.Add(this.DecryptButton);
+            this.FileControlGroupBox.Enabled = false;
+            this.FileControlGroupBox.Location = new System.Drawing.Point(109, 28);
+            this.FileControlGroupBox.Name = "FileControlGroupBox";
+            this.FileControlGroupBox.Size = new System.Drawing.Size(513, 61);
+            this.FileControlGroupBox.TabIndex = 8;
+            this.FileControlGroupBox.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(144, 32);
+            this.label2.Location = new System.Drawing.Point(6, 26);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 15);
-            this.label2.TabIndex = 7;
+            this.label2.TabIndex = 11;
             this.label2.Text = "Passphrase:";
+            // 
+            // PassphraseTextBox
+            // 
+            this.PassphraseTextBox.Location = new System.Drawing.Point(80, 22);
+            this.PassphraseTextBox.Name = "PassphraseTextBox";
+            this.PassphraseTextBox.Size = new System.Drawing.Size(212, 23);
+            this.PassphraseTextBox.TabIndex = 10;
+            this.PassphraseTextBox.UseSystemPasswordChar = true;
+            // 
+            // DecryptToFileButton
+            // 
+            this.DecryptToFileButton.Location = new System.Drawing.Point(379, 22);
+            this.DecryptToFileButton.Name = "DecryptToFileButton";
+            this.DecryptToFileButton.Size = new System.Drawing.Size(128, 23);
+            this.DecryptToFileButton.TabIndex = 9;
+            this.DecryptToFileButton.Text = "Dekrypt To File...";
+            this.DecryptToFileButton.UseVisualStyleBackColor = true;
+            this.DecryptToFileButton.Click += new System.EventHandler(this.DecryptToFileButton_Click);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(298, 22);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(75, 23);
+            this.DecryptButton.TabIndex = 8;
+            this.DecryptButton.Text = "Dekrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
             // 
             // DekryptForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(634, 411);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.PassphraseTextBox);
-            this.Controls.Add(this.DecryptToFileButton);
+            this.Controls.Add(this.FileControlGroupBox);
             this.Controls.Add(this.DecryptedContentsTextBox);
-            this.Controls.Add(this.DecryptButton);
             this.Controls.Add(this.SelectFileButton);
             this.Controls.Add(this.label1);
-            this.MinimumSize = new System.Drawing.Size(650, 200);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(650, 300);
             this.Name = "DekryptForm";
             this.Text = "Krypt Pad Dekrypter";
+            this.FileControlGroupBox.ResumeLayout(false);
+            this.FileControlGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,10 +149,11 @@
 
         private Label label1;
         private Button SelectFileButton;
-        private Button DecryptButton;
         private TextBox DecryptedContentsTextBox;
-        private Button DecryptToFileButton;
-        private TextBox PassphraseTextBox;
+        private GroupBox FileControlGroupBox;
         private Label label2;
+        private TextBox PassphraseTextBox;
+        private Button DecryptToFileButton;
+        private Button DecryptButton;
     }
 }
--- KryptPadDekrypter/DekryptForm.cs
+++ KryptPadDekrypter/DekryptForm.cs
@@ -5,38 +5,13 @@ namespace KryptPadDekrypter
 {
     public partial class DekryptForm : Form
     {
-
+        private const string KDF_FILTER = "Krypt Pad Document|*.kdf";
+        private const string JSON_FILTER = "JSON Text File|*.json";
         private Stream? _selectedFileStream = null;
+
         public DekryptForm()
         {
             InitializeComponent();
-        }
-
-        private void SelectFileButton_Click(object sender, EventArgs e)
-        {
-            using (var selectFileDialog = new OpenFileDialog())
-            {
-                selectFileDialog.Filter = "Krypt Pad Document|*.kdf";
-                var result = selectFileDialog.ShowDialog();
-                if (result == DialogResult.OK)
-                {
-                    _selectedFileStream = selectFileDialog.OpenFile();
-                    if (_selectedFileStream != null)
-                    {
-                        // Enable the decryption buttons
-                        PassphraseTextBox.Enabled = true;
-                        DecryptButton.Enabled = true;
-                        DecryptToFileButton.Enabled = true;
-                    }
-                }
-            }
-
-        }
-
-        private void DecryptButton_Click(object sender, EventArgs e)
-        {
-            // Decrypt the file and output its content into the textbox
-            DecryptedContentsTextBox.Text = DecryptFile();
         }
 
         /// <summary>
@@ -60,6 +35,29 @@ namespace KryptPadDekrypter
             return hashedPassphrase.Equals(profile.Key2);
         }
 
+        /// <summary>
+        /// Resets the stream position and reads the file to the end
+        /// </summary>
+        /// <returns></returns>
+        private string? ReadFileContents()
+        {
+            string? encryptedJSON = null;
+            if (_selectedFileStream != null)
+            {
+                // Reset stream position
+                _selectedFileStream.Position = 0;
+                // Read stream to its end
+                using var sr = new StreamReader(_selectedFileStream, leaveOpen: true);
+                encryptedJSON = sr.ReadToEnd();
+            }
+
+            return encryptedJSON;
+        }
+
+        /// <summary>
+        /// Decrypts the contents of the JSON file
+        /// </summary>
+        /// <returns></returns>
         private string? DecryptFile()
         {
             string? decryptedText = null;
@@ -68,71 +66,56 @@ namespace KryptPadDekrypter
             // Decrypt the file and output its content into the textbox
             if (_selectedFileStream != null)
             {
-                try
+
+                var encryptedJSON = ReadFileContents();
+                if (encryptedJSON != null)
                 {
-                    // Reset stream position
-                    _selectedFileStream.Position = 0;
-
-                    // Read all the contents of the file
-                    using (var sr = new StreamReader(_selectedFileStream, leaveOpen: true))
+                    // Next deserialize the profile into a Profile object
+                    var profile = JsonConvert.DeserializeObject<Profile>(encryptedJSON);
+                    if (profile != null && profile.Categories != null)
                     {
-                        var profileJson = sr.ReadToEnd();
-                        if (profileJson != null)
+                        // Check if the supplied passphrase matches the hash in the document
+                        if (!VerifyPassphrase(profile, passphrase))
                         {
-                            // Next deserialize the profile into a Profile object
-                            var profile = JsonConvert.DeserializeObject<Profile>(profileJson);
-                            if (profile != null && profile.Categories != null)
+                            throw new Exception("The passphrase you entered is not correct. Please try again.");
+                        }
+
+
+                        // Loop through each category and decrypt it
+                        foreach (var category in profile.Categories)
+                        {
+                            // If the category is not null, decrypt it and loop through its items
+                            if (category != null)
                             {
-                                // Check if the supplied passphrase matches the hash in the document
-                                if (!VerifyPassphrase(profile, passphrase))
+                                category.Name = Encryption.DecryptFromString(category.Name, passphrase);
+                                // Loop through each item and decrypt it
+                                if (category.Items != null)
                                 {
-                                    throw new Exception("The passphrase you entered is not correct. Please try again.");
-                                }
-
-
-                                // Loop through each category and decrypt it
-                                foreach (var category in profile.Categories)
-                                {
-                                    // If the category is not null, decrypt it and loop through its items
-                                    if (category != null)
+                                    foreach (var item in category.Items)
                                     {
-                                        category.Name = Encryption.DecryptFromString(category.Name, passphrase);
-                                        // Loop through each item and decrypt it
-                                        if (category.Items != null)
+                                        item.Name = Encryption.DecryptFromString(item.Name, passphrase);
+                                        item.Notes = Encryption.DecryptFromString(item.Notes, passphrase);
+                                        // Loop through all field values and decrypt them
+                                        if (item.Fields != null)
                                         {
-                                            foreach (var item in category.Items)
+                                            foreach (var field in item.Fields)
                                             {
-                                                item.Name = Encryption.DecryptFromString(item.Name, passphrase);
-                                                item.Notes = Encryption.DecryptFromString(item.Notes, passphrase);
-                                                // Loop through all field values and decrypt them
-                                                if (item.Fields != null)
-                                                {
-                                                    foreach (var field in item.Fields)
-                                                    {
-                                                        field.Name = Encryption.DecryptFromString(field.Name, passphrase);
-                                                        field.Value = Encryption.DecryptFromString(field.Value, passphrase);
+                                                field.Name = Encryption.DecryptFromString(field.Name, passphrase);
+                                                field.Value = Encryption.DecryptFromString(field.Value, passphrase);
 
-                                                    }
-                                                }
                                             }
                                         }
                                     }
                                 }
-
                             }
-
-                            // Serialize the decrypted output
-                            decryptedText = JsonConvert.SerializeObject(profile, Formatting.Indented);
-
                         }
 
                     }
 
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred while decrypting the file. {ex.Message}",
-                        "Error Decrypting", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    // Serialize the decrypted output
+                    decryptedText = JsonConvert.SerializeObject(profile, Formatting.Indented);
+
+
                 }
 
             }
@@ -140,6 +123,71 @@ namespace KryptPadDekrypter
             return decryptedText;
         }
 
-        
+        #region Events
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            using var ofd = new OpenFileDialog();
+            ofd.Filter = KDF_FILTER;
+            var result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _selectedFileStream = ofd.OpenFile();
+                if (_selectedFileStream != null)
+                {
+                    // Enable the decryption buttons
+                    FileControlGroupBox.Enabled = true;
+                    FileControlGroupBox.Text = ofd.SafeFileName;
+
+                }
+            }
+
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Decrypt the file and output its content into the textbox
+                DecryptedContentsTextBox.Text = DecryptFile();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while decrypting the file. {ex.Message}",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void DecryptToFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Decrypt the file and output its content into the textbox
+                var decryptedJSON = DecryptFile();
+                DecryptedContentsTextBox.Text = decryptedJSON;
+
+                // Write the decrypted contents to a file
+                using var sfd = new SaveFileDialog();
+                sfd.Filter = JSON_FILTER;
+                var result = sfd.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    // Open the file and write the contents
+                    using var sw = new StreamWriter(sfd.OpenFile());
+                    sw.Write(decryptedJSON);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while decrypting the file anmd writing to a new file. {ex.Message}",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+
     }
 }
